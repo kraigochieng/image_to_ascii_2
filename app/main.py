a7173edbc215d59e22b6720b782393f61550fc58
kraigochieng <kraigochieng@gmail.com>
2024-06-18 23:50:13 +0300

    removed models since it is not being used

--- app/main.py
+++ app/main.py
@@ -18,20 +18,20 @@ templates = Jinja2Templates(directory=templates_dir)
 static_dir = os.path.join(current_dir, "static")
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    context = {
-        "request": request
-    }
+    context = {"request": request}
 
     return templates.TemplateResponse("index.html", context=context)
+
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 app.include_router(image_to_ascii.router)
--- app/models/image.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-from fastapi import UploadFile
-from typing import Optional
-
-
-class ImageParametersModel(BaseModel):
-    image: UploadFile
-    font_size: Optional[int]
-    background_alpha: Optional[float]
